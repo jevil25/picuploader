f6befb5c7a9b8128e8ba38db32bde4622a66659d
AaronNazareth <4nm20cs005@nmamit.in>
2023-02-11 20:35:52 +0530

    styled navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./public/views/*.html", "./public/views/*.hbs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom_black': "#0F172A",
+        'custom_white': "#f8fafc",
+        'custom_border' : "#2A364A"
+      }
+    },
   },
   plugins: [],
 }
