ca83603f1240c45882d8613449e990e1a2feeba4
AaronNazareth <4nm20cs005@nmamit.in>
2023-02-11 22:27:34 +0530

    added responsiveness for navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,22 @@ module.exports = {
         "hover_color":"#1e293b"
       }
     },
+    screens: {
+      'laptop': { 'max': '1024px' },
+      // => @media (min-width: 1024px) { ... }
+
+      'tablet': { 'max': '768px' },
+      // => @media (min-width: 768px) { ... }
+
+      'mobile': { 'max': '640px'},
+      // => @media (min-width: 640px) { ... }
+
+      'xl': { 'max': '1280px' },
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': { 'max': '1536px' },
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
